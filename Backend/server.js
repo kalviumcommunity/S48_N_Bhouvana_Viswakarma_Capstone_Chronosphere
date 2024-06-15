0367473d8dee26098005fd4ef58eac791248da4c
Bhuvan <bhouvana@gmail.com>
2024-06-15 16:04:48 +0530

    GET endpoint created

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/my-endpoint', (req, res) => {
+    res.json({ message: 'Hello, This is my Capstone Project' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
